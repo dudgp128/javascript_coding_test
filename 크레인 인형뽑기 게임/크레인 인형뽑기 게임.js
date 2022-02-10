2d443739f8ef8a3af2d881acc957fe1c116fa893
조영혜 <20181048@sungshin.ac.kr>
2022-02-10 20:35:55 +0900

    출력문 지우기

--- 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -7,24 +7,19 @@ function solution(board, moves) {
         if (board[board.length - 1][j] != 0) {
             for (let L in board) {
                 if (board[L][j] != 0) {
-                    //console.log(L,j)
                     if (array.length > 0) {
                         if (board[L][j] == array[array.length - 1]) {
-                            //console.log(board[L][j])
                             board[L][j] = 0;
                             answer += 2
                             array.pop();
-                            //console.log('pop:',array)
                         }
                         else {
                             array.push(board[L][j]);
-                            //console.log('push:',array)
                             board[L][j] = 0;
                         }
                     }
                     else {
                         array.push(board[L][j]);
-                        //console.log('초기',array)
                         board[L][j] = 0;
                     }
                     break;
@@ -32,6 +27,5 @@ function solution(board, moves) {
             }
         }
     }
-    console.log(answer)
     return answer;
 }
