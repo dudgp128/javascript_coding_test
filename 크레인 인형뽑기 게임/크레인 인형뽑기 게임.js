09f6596e44357834d5e85af3ccc5a90e6dc35c10
조영혜 <20181048@sungshin.ac.kr>
2022-02-10 20:27:22 +0900

    필요없는 continue 삭제

--- 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,39 +1,36 @@
 function solution(board, moves) {
     var answer = 0;
-    var array=[];
-    var j=0;
-    for(let i of moves){
-        j=i-1;
-        if(board[board.length-1][j]!=0){
-            for(let L in board){
-                if(board[L][j]!=0){
+    var array = [];
+    var j = 0;
+    for (let i of moves) {
+        j = i - 1;
+        if (board[board.length - 1][j] != 0) {
+            for (let L in board) {
+                if (board[L][j] != 0) {
                     //console.log(L,j)
-                    if(array.length>0){
-                        if(board[L][j]==array[array.length-1]){
+                    if (array.length > 0) {
+                        if (board[L][j] == array[array.length - 1]) {
                             //console.log(board[L][j])
-                            board[L][j]=0;
-                            answer+=2
+                            board[L][j] = 0;
+                            answer += 2
                             array.pop();
                             //console.log('pop:',array)
                         }
-                        else{
+                        else {
                             array.push(board[L][j]);
                             //console.log('push:',array)
-                            board[L][j]=0;
+                            board[L][j] = 0;
                         }
                     }
-                    else{
+                    else {
                         array.push(board[L][j]);
                         //console.log('초기',array)
-                        board[L][j]=0;
+                        board[L][j] = 0;
                     }
-                break;
+                    break;
                 }
-                else
-                    continue;
             }
-        }else
-            continue;
+        }
     }
     console.log(answer)
     return answer;
