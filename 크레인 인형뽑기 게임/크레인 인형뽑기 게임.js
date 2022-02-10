df4a1a26d12359d4d8708cf9486b7af22e9a10ac
조영혜 <20181048@sungshin.ac.kr>
2022-02-10 20:25:20 +0900

    2022-02-10 코딩테스트

---
+++ 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,40 @@
+function solution(board, moves) {
+    var answer = 0;
+    var array=[];
+    var j=0;
+    for(let i of moves){
+        j=i-1;
+        if(board[board.length-1][j]!=0){
+            for(let L in board){
+                if(board[L][j]!=0){
+                    //console.log(L,j)
+                    if(array.length>0){
+                        if(board[L][j]==array[array.length-1]){
+                            //console.log(board[L][j])
+                            board[L][j]=0;
+                            answer+=2
+                            array.pop();
+                            //console.log('pop:',array)
+                        }
+                        else{
+                            array.push(board[L][j]);
+                            //console.log('push:',array)
+                            board[L][j]=0;
+                        }
+                    }
+                    else{
+                        array.push(board[L][j]);
+                        //console.log('초기',array)
+                        board[L][j]=0;
+                    }
+                break;
+                }
+                else
+                    continue;
+            }
+        }else
+            continue;
+    }
+    console.log(answer)
+    return answer;
+}
